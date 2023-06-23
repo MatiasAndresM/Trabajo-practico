7d308d018590448b9f2bc460073311c3d0abc8b6
MatiasAndresM <matiasa.m@hotmail.com>
2023-06-23 10:53:17 -0300

    Cambio Dashboard

--- src/app/cpmponente/dashboard/dashboard.component.ts
+++ src/app/cpmponente/dashboard/dashboard.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  textEvent: string;
+  imagenDashboard: string;
 
+  constructor(){
+    this.textEvent="User"
+    this.imagenDashboard= "https://www.nicepng.com/png/detail/141-1415492_bootstrap-bootstrap-4-icon-png.png"
+  }
+  cambiarTexto(): void{
+    this.textEvent="Matias Andres Maza"
+  }
+  cambiarImagen(): void{
+    this.imagenDashboard= "https://1.bp.blogspot.com/-MwJI22_Ek_0/XJQEjL9WGjI/AAAAAAAAJSs/Kd9WAGTItDoTRoaIFLE8qwOrj3STIMbfQCK4BGAYYCw/s1600/logo%2Bangular%2Bicon.png"
+  }
 }
